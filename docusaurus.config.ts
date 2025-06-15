f9f435b9e20ecbe0ac5be3b58ab6733c095e5a87
VietCardanoCommunity <alexeikotinov123@gmail.com>
2025-06-15 16:15:39 +0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://VietCardanoCommunity.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cardanodoc',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
